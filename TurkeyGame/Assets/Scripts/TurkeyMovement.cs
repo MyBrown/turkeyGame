f2b37acb4cfa3f032a30f4fe04dde3bf196fdf24
MyBrown <myaisa.brown03@gmail.com>
2025-11-15 15:04:50 -0500

    double jump finalized

--- TurkeyGame/Assets/Scripts/TurkeyMovement.cs
+++ TurkeyGame/Assets/Scripts/TurkeyMovement.cs
@@ -4,19 +4,36 @@ using UnityEngine.InputSystem;
 public class TurkeyMovement : MonoBehaviour
 {
     public float jumpforce;
+    private float extraJump;
+    private bool isGrounded;
+    private Rigidbody2D turkeyRigidbody;
+    private BoxCollider2D turkeyCollider;
+    public BoxCollider2D floorCollider;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        turkeyRigidbody = GetComponent<Rigidbody2D>();
+        turkeyCollider = GetComponent<BoxCollider2D>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (turkeyCollider.IsTouching(floorCollider))
         {
-            GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpforce);
+            extraJump = 1;
         }
-        
+            
+        if (Input.GetKeyDown(KeyCode.Space) && turkeyCollider.IsTouching(floorCollider))
+        {
+            turkeyRigidbody.linearVelocity = Vector2.up * jumpforce;
+        }    
+        else if (Input.GetKeyDown(KeyCode.Space) && extraJump > 0)
+        {
+            turkeyRigidbody.linearVelocity = Vector2.up * jumpforce;
+            extraJump--;
+        }
     }
 }
